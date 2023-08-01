f7942ef8d9dfa6a90e4787398df0ed54bf9dfd40
Valeriereds <valeriereds@gmail.com>
2023-07-31 22:46:12 -0500

    Added index.js routes and helper folders

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// needed for app.use and routes
+const express = require('express');
+
+const notesRouter = require('./notes');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+module.exports = app;
